e3c7d1d85783b9e59a623b62008159479795bbdb
Evan <evan@brucemcrooster.dev>
2025-10-26 16:25:40 -0400

    add md4c

--- Package.swift
+++ Package.swift
@@ -15,7 +15,23 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "WrenWrite"
-        )
+            name: "WrenWrite",
+            dependencies: [
+                "md4c_html"
+            ]
+        ),
+        .target(
+            name: "md4c_html",
+            path: "Sources/md4c",
+            sources: [
+                "src/entity.h",
+                "src/md4c.h",
+                "src/md4c-html.h",
+                "src/entity.c",
+                "src/md4c.c",
+                "src/md4c-html.c",
+            ],
+            publicHeadersPath: "."
+        ),
     ]
 )
